0b9f8689e1506332629cd1f16fff4f087ebbc496
Q. Haro <harog2@unlv.nevada.edu>
2025-08-07 14:47:01 -0700

    Pass QEMU an offset as arg2

--- simul/bin/qemu.sh
+++ simul/bin/qemu.sh
@@ -7,9 +7,11 @@ opts=-nographic 	# disable the graphical output
 opts+=" -machine virt"	# the RISC-V simulated machine
 opts+=" -bios none"	# Prevent the OpenSBI bios from being loaded
 opts+=" -m 128M"	# 128 megabytes of memory
-opts+=" -smp 8"		# The number of cores (harts)
+opts+=" -smp 1"		# The number of cores (harts)
 # The -gdb option does not work, I do not know why
 # opts+=" -gdb tcp:4321"	# Specify the GDB remote port
+# opts+=" -s"
+# opts+=" -S"         # Connect GDB to qemu
 
 # libcache enable and parameters
 opts+=" --plugin ${PLUGIN}"
@@ -17,8 +19,9 @@ opts+=',dcachesize=16384,dassoc=2,dblksize=32'
 opts+=',icachesize=16384,iassoc=2,iblksize=32'
 opts+=' -d plugin'
 opts+=' -D cache.log'
-
+opts+=" -device loader,addr=0x80100000,data=$2,data-len=4"
 opts+=" -kernel $1"	# the kernel to add
+
 
 echo "Invoking: ${QEMU} ${opts}"
 ${QEMU} ${opts}
